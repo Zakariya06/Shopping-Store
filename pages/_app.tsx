91080fd67f909e4cb59b70bb222224964e5dc869
Zakariya06 <zakariyakhanamar00@gmail.com>
2025-08-05 17:13:35 +0500

    feat: add app and main root file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import ProtectedRoute from "@/hoc/ProtectedRoute";
+import { AuthContextProvider } from "@/context/authContext";
+import { Toaster } from "react-hot-toast";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <AuthContextProvider>
+      <ProtectedRoute>
+        <Component {...pageProps} />
+        <Toaster position="top-right" />
+      </ProtectedRoute>
+    </AuthContextProvider>
+  );
 }
